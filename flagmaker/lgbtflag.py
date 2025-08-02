a657b3b17d1f02ae97b62631af97700da5ab465d
abbiistabbii <142050006+abbiistabbii@users.noreply.github.com>
2025-08-02 19:18:42 +0100

    Update lgbtflag.py
    Simplified the code

--- flagmaker/lgbtflag.py
+++ flagmaker/lgbtflag.py
@@ -5,21 +5,17 @@ turtle.seth(0)
 colours = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 x = 0
+posx = 0
+posy = 0
 
-def stripe():
-	for x in range(10):
-		turtle.forward(200)
-		turtle.right(90)
-		turtle.forward(1)
-		turtle.right(90)
-		turtle.forward(200)
-		turtle.left(90)
-		turtle.forward(1)
-		turtle.left(90)
 
-for x in range (6):
+for x in range(6):
 	turtle.color(colours[x])
-	stripe()
+	for x in range(40):
+		turtle.forward(400)
+		posy -= 1
+		turtle.teleport(posx, posy)
+
 	x += 1
 
 turtle.done()
